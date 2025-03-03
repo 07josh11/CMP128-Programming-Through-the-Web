17c679c070ccd0537f8effb8adc6f3b9c340f037
07josh11 <8ghostwolf8@gmail.com>
2025-03-03 17:52:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let fruits ={"grapes", "apples", "pears", "oranges"};
 let fruit = fruits[0]; //first fruit
+function logFirstFruit()
+{
+  let randomNumber= math.floor(math.random()*fruits.length);
+  console.log(fruits[);
+}
+logFirstFruit();
